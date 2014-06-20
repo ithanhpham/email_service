b219a101f8b56b82725e202391a70058b5b05a16
Thanh Pham <thanh@thismoment.com>
2014-06-20 15:37:34 -0700

    set up outline

---
+++ helpers/mailgun_service.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * Author: Thanh Pham
+ * usage:  call Mailgun's services to send mail with params
+ */
+
+class mailgun {
+    
+        //set api keys
+
+        //set any required api info
+    
+    //method to send an email
+    public function send(){
+    
+    }
+}

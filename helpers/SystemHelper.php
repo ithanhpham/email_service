344fd749df068b6668b99807bc91ec0a8f76fa5c
Thanh Pham <thanh@thismoment.com>
2014-07-07 22:59:48 -0700

    Revert "adding unit tests"
    This reverts commit d514d0df7686e7306c9840e46ac697215be94ea2.

--- helpers/SystemHelper.php
+++ helpers/SystemHelper.php
@@ -1,19 +1,17 @@
 <?php
+if(!defined('ACCESS') ) { die('permission denied');}
 
 /* helper method for status codes */
 
 class SystemHelper {
     
-    protected function get_status_code($en, $em, $source = null) {
+    public function get_status_code($en, $em, $source = null) {
         
         if(isset($en) && isset($em) ){
             
-            header($_SERVER["SERVER_PROTOCOL"] . $en);
-            header("Content-Type: application/json"); 
-                    
             if(isset($source)) {
                 if($en >= 200 && $en < 300) {
-                    $this->status = array('status' => $en, 'source' => $source, 'result' => $em);                   
+                    $this->status = array('status' => $en, 'source' => $source, 'result' => $em);
                     return($this->status);
 
                 } else {
@@ -34,14 +32,8 @@ class SystemHelper {
         }            
     }
     
-    protected function json_header($status_code){
-            
-        header($_SERVER["SERVER_PROTOCOL"] . $status_code);
-        header("Content-Type: application/json");
-    }    
-    
     //logger
-    protected function simple_logger($status, $service, $email_data){
+    public function simple_logger($status, $service, $email_data){
 
         //build log
         $log_data = $status . ', ' . $service . ', ' . $email_data['from_email'] . ', ' . $email_data['to_email'] . ', ' . time() . "\n";

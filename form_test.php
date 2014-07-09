9c35627b346e8a2216bbe97a60642b796dcca637
Thanh Pham <thanh@thismoment.com>
2014-07-09 07:46:55 -0700

    add google analytics

--- form_test.php
+++ form_test.php
@@ -106,7 +106,16 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <title>ACME email form</title>
 <link rel="stylesheet" type="text/css" href="form_test/view.css" media="all">
 <script type="text/javascript" src="form_test/view.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-28096092-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body id="main_body" >
 
